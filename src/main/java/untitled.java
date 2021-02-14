fb1970f849c541cde305317e70b0155e55c9dfc2
abrahamcasmay <vcastorelam1600@alumno.ipn.mx>
2021-02-14 03:00:39 -0600

    NUevo contenido

--- src/main/java/untitled.java
+++ src/main/java/untitled.java
@@ -1,5 +1,9 @@
 public class untitled {
     public static void main(String args[]){
         System.out.println("Hola bb");
+        for (int x=1;x<=10;x++){
+            System.out.println("Hola bb");
+
+        }
     }
 }
